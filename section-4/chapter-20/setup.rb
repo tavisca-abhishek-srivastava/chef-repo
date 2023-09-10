eb4c255ea88fcb8625d398be5ba109220c0d184a
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2023-09-10 14:05:14 +0530

    Added chapter-4

---
+++ section-4/chapter-20/setup.rb
@@ -0,0 +1,15 @@
+# ~ setup.rb
+
+package 'tree' do
+    action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+    content 'This server is the property of ...'
+    owner 'root'
+    group 'root'
+    # mode '0755'
+    action :create
+end
